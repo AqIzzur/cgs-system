2a2a9e367a70567cd2fe1013c1dc33007c7f59a2
AqIzzur11 <rohmanizzur11@gmail.com>
2024-12-26 01:36:19 +0700

    Bismillah Upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,131 @@
+@extends('main')
+@section('content')
+@php
+$tanggal = date('d-m-Y ');
+// $timestamp    = date('H:i:s');
+// $time    = time('')                               
+@endphp
+<section id="header" class="pt-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="date rounded-start-5 rounded-end-3 bg-success bg-opacity-50 d-flex">
+                            <div class="date-circle me-3 d-flex justify-content-center align-items-center bg-white">
+                                <i class="fa fa-calendar-days fa-xl fa-fade"></i>
+                            </div>
+                            <h4 class=" m-auto me-3 text-dark">{{ $tanggal }} </h4>
+                        </div>
+                    </div>
+                    <div class="col d-flex">
+                        {{-- <h4 class="text-end"><span id="real-time" class="text-primary"></span></h4> --}}
+                        <div class=" date ms-auto rounded-start-3 rounded-end-5 bg-info bg-opacity-50 d-flex">
+                            <h4 class=" m-auto mx-3 text-dark"><span id="real-time" class=""></h4>
+                            <div class="date-circle  d-flex justify-content-center align-items-center bg-white">
+                                <i class="fa-regular fa-clock fa-xl fa-spin"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <div class="row text-utama">
+                <div class="col-10 pt-5">
+                    <h2 class="text-utama2 poppins-regular">Team Creative</h3>
+                    <h1 class="text-utama1 fw-bold poppins-regular">Cipta Graha Software</h2>
+                </div>
+                <div class="col-2">
+                    {{-- <p id="real-time"></p> --}}
+                    <img class="logo-cgs1 rounded-3" src="{{ asset('asset/image/logo-cgs.png') }}" alt="">
+                </div>
+            </div>
+            
+            <div class="row button-sign">
+                <div class="col-12 d-flex justify-content-between ">
+                    <a href="#" class="btn btn-primary bg-opacity-50 fw-bold btn1 p-3 text-uppercase">Sign In</a>
+                    <a href="#" class="btn btn-primary bg-opacity-50 fw-bold btn2 p-3 text-uppercase">Sign Up</a>
+    
+                </div>
+
+            </div>
+        </div>
+    </section>
+<section id="body" class="pb-5">
+    <div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="fw-bold text-center poppins-bold">Welcome Team CGS</h1>
+            <hr class="border border-dark border-3 mx-auto" style="width: 35%; margin-top:-10px;">
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col">
+            <div class="text-justify poppins-reguler text-white" style="font-size: 20px">"System Team Creative CGS" adalah sebuah sistem yang dirancang khusus untuk mempermudah proses pelaporan dan pengelolaan aktivitas kreatif di Cipta Graha Software. Sistem ini memungkinkan anggota tim untuk melaporkan progres pekerjaan mereka secara real-time, memantau status tugas yang sedang berjalan, serta berbagi feedback atau ide-ide baru secara efisien. Dengan fitur yang mudah digunakan dan integrasi yang seamless, sistem ini membantu meningkatkan koordinasi, transparansi, dan produktivitas dalam tim creative CGS, memastikan setiap proyek berjalan sesuai rencana dan memenuhi standar kualitas yang tinggi.</div>
+        </div>
+    </div>
+    <div class="row button-asset mt-5">
+        <div class="col-12 d-flex justify-content-between ">
+            <a href="#" class="btn btn-secondary bg-opacity-50 fw-bold btn-aset p-3 text-uppercase">Asset</a>
+            <a href="#" class="btn btn-secondary bg-opacity-50 fw-bold btn-hasil p-3 text-uppercase">Hasil</a>
+
+        </div>
+
+    </div>
+</div>
+</section>
+<section id="footer" class="">
+    <div class="container">
+    <div class="row py-4">
+        <div class="col">
+            <img class="logo-cgs2 rounded" src="{{ asset('asset/image/logo-cgs.png') }}" alt="">
+            <h2 class="poppins-regular mt-3 text-white" style="font-size: 20px">Cipta Graha Software - Solusi Terbaik Bisnis Anda</h2>
+            <h4 class="text-white" style="font-size:12px">Perum Wiratama 1 No.4, Pekuncen, Kec. Wiradesa <br> Kabupaten Pekalongan, Jawa Tengah 51153</h4>
+        </div>
+        <div class="col d-flex justify-content-end flex-column">
+            <iframe class="ms-auto text-end rounded" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.032481053019!2d109.6257864749962!3d-6.886712893112283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e70265ac7f7b3cd%3A0x8231cc2a3cee2797!2sCV.%20Cipta%20Graha%20Software!5e0!3m2!1sid!2sid!4v1735145506031!5m2!1sid!2sid" 
+            width="305" 
+            height="150.4" 
+            style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+            <div class="ms-auto  mt-3">
+                <a href="https://www.youtube.com/@ciptagrahasoftware" class=" me-4 text-decoration-none">
+                    <img src="{{ asset('asset/icon-sosmed/yt.png') }}" alt="" class="icon-sos1">
+                </a>
+                <a href="https://api.whatsapp.com/send/?phone=6282124242386&text&type=phone_number&app_absent=0" class="me-4 text-decoration-none">
+                    <img src="{{ asset('asset/icon-sosmed/wa2.png') }}" alt="" class="icon-sos2 ">
+                </a>
+                <a href="https://www.facebook.com/CiptaGrahaSoftware" class="me-4 text-decoration-none">
+
+                    <img src="{{ asset('asset/icon-sosmed/fb2.png') }}" alt="" class="icon-sos2 ">
+                </a>
+                <a href="https://www.instagram.com/ciptagrahasoftware?igsh=MThlNno4d2N1Z3Ficg==" class="me-4 text-decoration-none">
+
+                    <img src="{{ asset('asset/icon-sosmed/ig2.png') }}" alt="" class="icon-sos2 ">
+                </a>
+                <a href="https://www.ciptagrahasoftware.com" class="text-decoration-none">
+                    <img src="{{ asset('asset/icon-sosmed/web1.png') }}" alt="" class="icon-sos2 ">
+
+                </a>
+            </div>
+        </div>
+    </div>
+    <p class="text-center poppins-regular text-white mb-3">
+        © 2024 M. Izzur Rohman  . All rights reserved.
+    </p>
+    </div>
+</section>
+
+
+
+
+    <script>
+        function updateClock() {
+            const now = new Date();
+            const time = now.toLocaleTimeString('en-GB'); // Format H:i:s
+            document.getElementById('real-time').textContent = time;
+        }
+
+        // Jalankan setiap detik
+        setInterval(updateClock, 1000);
+
+        // Panggil pertama kali
+        updateClock();
+    </script>
+@endsection
---
+++ resources/views/main.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Tambahkan token CSRF dalam request POST -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Team Cretive</title>
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="{{ asset('asset/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('asset/css/awesome/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('asset/css/style-custom.css')  }}">
+    <!-- Tambahkan jQuery CDN di <head> atau sebelum </body> -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+
+</head>
+<body>
+    @yield('content') 
+    <!-- Alert Modal -->
+    @if (session('success'))
+    <div class="modal fade alert-modal" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header bg-success bg-opacity-50">
+                    <h5 class="modal-title " id="successModalLabel">Berhasil</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-3">
+                            <i class="ms-3 fa-solid fa-circle-check fa-4x" style="color: #63E6BE;"></i>
+                        </div>
+                        <div class="col ">
+                            <p class="text-uppercase my-auto fs-3 fw-bold mt-2">{{ session('success') }}</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-success" data-bs-dismiss="modal">OK</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+    @if (session('error'))
+    <div class="modal fade alert-modal" id="errorModal" tabindex="-1" aria-labelledby="errorModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header bg-danger bg-primary-50">
+                    <h5 class="modal-title text-white " id="errorModalLabel">Gagal</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row mb-3">
+                        <div class="col-3">
+                            <i class="fa-solid fa-circle-exclamation fa-4x ms-3" style="color: #ff0000;"></i>
+                        </div>
+                        <div class="col ">
+                            <p class="text-uppercase my-auto fs-3 fw-bold mt-2">{{ session('error') }}</p>
+
+                        </div>
+                        
+                    {{-- <p class="text-uppercase"></p> --}}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger " data-bs-dismiss="modal">OK</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+
+    <!-- Bootstrap JS -->
+    
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            // Cek session flash untuk modal success
+            @if (session('success'))
+                var successModal = new bootstrap.Modal(document.getElementById('successModal'));
+                successModal.show();
+            @endif
+    
+            // Cek session flash untuk modal error
+            @if (session('error'))
+                var errorModal = new bootstrap.Modal(document.getElementById('errorModal'));
+                errorModal.show();
+            @endif
+        });
+    </script>
+    
+
+<script src="{{ asset('asset/js/bootstrap.bundle.js') }}"></script>
+<script src="{{ asset('asset/js/awesome/all.min.js') }}"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</body>
+</html>
+{{-- @izzur.akun1426 --}}
