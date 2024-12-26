6e45bbc0303af06f28a88ffcbc5fdf33361a2db7
AqIzzur <izzurr1116@gmail.com>
2024-12-26 15:14:28 +0700

    Todolist

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodolistController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/todolist', [TodolistController::class, 'view'])->name('todolist.view');
